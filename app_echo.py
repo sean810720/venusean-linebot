4f50446a65037f9d937b319995395ffe427f2819
Sean <sean.chen@starclinic.com.tw>
2022-03-02 11:02:47 +0800

    init

--- app_echo.py
+++ app_echo.py
@@ -43,12 +43,12 @@ def echo(event):
         if event.message.text == "大盤指數":
             line_bot_api.reply_message(
                 event.reply_token,
-                TextSendMessage(text="目前大盤指數: 18000")
+                TextSendMessage(text="目前大盤指數萬八上看兩萬")
             )
         elif event.message.text == "美元匯率":
             line_bot_api.reply_message(
                 event.reply_token,
-                TextSendMessage(text="目前美元匯率: 28.01")
+                TextSendMessage(text="目前美元匯率 28.01")
             )
         elif event.message.text == "小咪" or event.message.text == "無敵小咪":
             line_bot_api.reply_message(
