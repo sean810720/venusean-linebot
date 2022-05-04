6a3d2402191ebaa9ef43f15798393990edea1c36
Sean <sean.chen@starclinic.com.tw>
2022-05-04 13:56:44 +0800

    加上大盤指數

--- venusean-linebot.py
+++ venusean-linebot.py
@@ -75,7 +75,7 @@ def echo(event):
             stock_rate = soup.select(".clr-rd")[1].text.strip()
 
             # 組出結果
-            result = "目前大盤指數 {} ({})".format(stock_index, stock_rate)
+            result = "目前台股大盤指數 {} ({})".format(stock_index, stock_rate)
 
         # 美元匯率
         elif "美元" in event.message.text or "美金" in event.message.text:
